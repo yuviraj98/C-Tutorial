837fdebd54531ccb9b3862f59f18ee1db061bb43
yuvraj.banger <bangaryuviraj1998@gmail.com>
2022-01-08 16:29:05 +0530

    ASCII code

---
+++ ASCII code/ASCII code.cpp
@@ -0,0 +1,20 @@
+
+
+#include <iostream>
+using namespace std;
+// ASCII code stand for American standard code for information interchange 
+//value are range for  0 to 127
+
+int main()
+{
+	cout << (int)'a' << endl;
+	cout << int('a') << endl;
+	cout << (int)'A' << endl;
+	cout << int('A') << endl;
+	cout << int('z') << endl;
+	cout << int('$') << endl;
+	return 0;
+
+   
+}
+
