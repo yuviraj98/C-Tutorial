615031eb3026616cb1659efb665b770a730e52a1
yuvraj.banger <bangaryuviraj1998@gmail.com>
2022-01-17 10:45:08 +0530

    ASCII message

---
+++ simple if statement problem/simple if statement problem.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int number;
+	cout << "enter a integer :";
+	cin >> number;
+	if (number > 0)
+
+	{
+		cout << " you enter a positive integer " << number << endl;
+	}
+	cout << " thise stetment always executed";
+
+
+}
