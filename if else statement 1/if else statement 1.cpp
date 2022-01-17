bea776a409869005a0ca3cefa9f5d404ac4f1909
yuvraj.banger <bangaryuviraj1998@gmail.com>
2022-01-17 12:11:47 +0530

    if else statement

---
+++ if else statement 1/if else statement 1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int number;
+	cout << " enter a integer ";
+	cin >> number;
+	if (number >= 0)
+	{
+		cout << "The number is positive :";
+	}
+	else
+	{
+		cout << " The number  is negative :";
+
+	}
+	return 0;
+}
